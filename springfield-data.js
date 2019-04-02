8c20f755854966fab04686125f693ea16827f2d8
ckb <ckingbailey@gmail.com>
2019-04-01 21:58:26 -0700

    add mayor quimby to map

--- springfield-data.js
+++ springfield-data.js
@@ -1,5 +1,30 @@
-const springfieldData = {
-    "nodes": {},
-    "edges": {},
-    "captions": {}
+var springfieldData = {
+    "title": "Springfield Influence Map",
+    "graph": {
+        "nodes": {
+            1: {
+                id: 1,
+                display: {
+                    name: 'Mayor Quimby',
+                    scale: 1,
+                    status: 'highlighted',
+                    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Mayor_Quimby.png/220px-Mayor_Quimby.png',
+                    url: '',
+                    x: 0,
+                    y: 0
+                }
+            }
+        },
+        "edges": {},
+        "captions": {
+            1: {
+                id: 1,
+                display: {
+                    text: 'Mayor Quimby',
+                    x: 1,
+                    y: 1
+                }
+            }
+        }
+    }
 }
